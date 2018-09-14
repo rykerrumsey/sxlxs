a3aad6575c008cab1eb595f363b5ff7a5117458c
Ryker Rumsey <rykerrumsey@outlook.com>
2018-09-14 14:45:19 -0600

    Added jinja2 templates

--- sxlxs/main.py
+++ sxlxs/main.py
@@ -1,15 +1,19 @@
+import os
+import requests
+import json
 
 from cement import App, TestApp, init_defaults
 from cement.core.exc import CaughtSignal
 from .core.exc import mainError
+from cement.utils import fs
 from .controllers.base import Base
+from openpyxl import Workbook, load_workbook
+from jinja2 import Template, FileSystemLoader, Environment
 
 # configuration defaults
-CONFIG = init_defaults('todo')
-CONFIG['todo']['foo'] = 'bar'
+CONFIG = init_defaults('sxlxs')
 
-
-class main(App):
+class Sxlxs(App):
     """sxlxs primary application."""
 
     class Meta:
@@ -39,22 +43,97 @@ class main(App):
 
         # set the output handler
         output_handler = 'jinja2'
-
+      
         # register handlers
         handlers = [
             Base
         ]
+        
+    templateLoader = FileSystemLoader('./templates')
+    templateEnv = Environment(loader=templateLoader)
+    TEMPLATE_FILE = "main_template.html"
+        
+    if os.path.exists('./data/bookings.xlsx'):
+        print("Bookings exists")
+    else:
+        print("Bookings does not exist")
 
+    wb = load_workbook("./data/bookings.xlsx")
+    client_sheet = wb["Clients"]
+    print(client_sheet)
+    template = templateEnv.get_template(TEMPLATE_FILE)
 
-class mainTest(TestApp,main):
-    """A sub-class of main that is better suited for testing."""
+    print(wb.sheetnames)
+    
+    def get_profiles():
+        #temporary because we can get the amount 
+        amount = 20
 
-    class Meta:
-        label = 'sxlxs'
+        r = requests.get('https://randomuser.me/api/?exc=login,gender,name,email,registered,id,nat&results=' + str(amount)  + '&seed=abc')
 
+        #must parse json before accessing the object
+        json_data = json.loads(r.text)
+
+        #clean up object before sending out
+        results = json_data['results']
+        print(results) 
+        return results
+
+    def get_clients(client_sheet):
+        name_list = []
+        for row in client_sheet.iter_rows('A{}:A{}'.format(client_sheet.min_row, client_sheet.max_row)):
+            for cell in row:
+                name_list.append(cell.value)
+        
+        #get rid of column title
+        del name_list[0]
+        
+        client_list = []
+    
+        print(name_list)
+        
+        for clients in name_list:
+            first_name = clients.split()[0]
+            last_name = clients.split()[1]
+            
+            print(first_name)
+            print(last_name)
+            
+            current_client = {}
+            current_client["first"] = first_name
+            current_client["last"] = last_name
+
+            client_list.append(current_client)
+            
+        print(client_list)
+
+        return client_list
+    client_data = get_clients(client_sheet)
+    
+    
+    def extract_pictures(profiles):
+        pictures = []
+        
+        print(profiles)
+
+        for client in profiles:
+            print("-------------")
+            print(client['picture']['large'])
+            print("-------------")
+            pictures.append(client['picture']['large'])
+
+        print(pictures)
+        return pictures
+
+    output = template.render(workbook=wb.sheetnames, clients=client_data, number_of_worksheets=len(wb.sheetnames), number_of_clients=len(client_data), pictures=extract_pictures(get_profiles()))
+
+    # create the index.html file if it doesn't exist and write the rendered template to it
+    html_file = open("./data/index.html", 'w')
+    html_file.write(output)
+    html_file.close()
 
 def main():
-    with main() as app:
+    with Sxlxs() as app:
         try:
             app.run()
 
